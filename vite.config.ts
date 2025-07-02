fec25484280cdcbdb91af32cb2dae17ed01c5d37
Lek Hsiang Hui <xianghui83@gmail.com>
2025-07-02 22:43:43 +0800

    update vite.config.ts to work with github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+  const env = loadEnv(mode, ".", "");
+  return {
+    define: {
+      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+    base: "/wordcraft/",
+  };
 });
